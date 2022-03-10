76ecfc92b8e8d8629eb208abc34c42fee90744a4
MainaWambui <@wambui.maina115@gmail.com>
2022-03-10 05:25:52 -0800

    Updated using vi and Betty checker

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase given using puts "Programming"
+ * Return: 0 if exited properly and non zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
