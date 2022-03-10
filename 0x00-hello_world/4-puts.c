8eb222c872a3256c610621aa594b1922e7d09653
MainaWambui <@wambui.maina115@gmail.com>
2022-03-10 05:07:18 -0800

    Amended using vi and used Betty in terminal

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints the phrase given
+ * Return: 0 if success, non zero if failed
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
